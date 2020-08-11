b0645451c54fcff81273d3ce65c2f0f2a9426db2
Vineeth B <vineethbc.dev@gmail.com>
2020-08-11 08:27:29 +0530

    authorization of users using bcrypt

--- authServer.js
+++ authServer.js
@@ -2,25 +2,71 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 app.use(express.json());
 
 let refreshTokens = [];
 
-app.post("/login", (req, res) => {
-  // TODO authenticate user
+const users = [
+  {
+    username: "Alex",
+    password: "password"
+  },
+  {
+    username: "Brad",
+    password: "123456"
+  }
+];
 
-  const username = req.body.username;
-  const user = { name: username };
+let usersHashed = [];
 
-  //require('crypto').randomBytes(64).toString('hex')
+(() => {
+  // encoding user passwords. generally done during user creation
+  try {
+    usersHashed = users.map((user) => {
+      const hashedPassword = bcrypt.hashSync(user.password, 10);
+      user.password = hashedPassword;
+      return user;
+    });
 
-  const accessToken = generateAccessToken(user);
-  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
-  refreshTokens.push(refreshToken);
-  res.json({ accessToken, refreshToken });
+    // generate secret tokens during initialize rather than default in .env
+    // require('crypto').randomBytes(64).toString('hex')
+    process.env.ACCESS_TOKEN_SECRET = crypto.randomBytes(64).toString("hex");
+    process.env.REFRESH_TOKEN_SECRET = crypto.randomBytes(64).toString("hex");
+  } catch (e) {
+    console.log(e);
+  }
+})();
+
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!password) {
+      return res.status(400).send("password is required");
+    }
+    const user = usersHashed.find((user) => {
+      return user.username === username;
+    });
+    if (!user) {
+      return res.status(400).send("Cannot find user");
+    }
+    // authenticate user
+    if (await bcrypt.compare(password, user.password)) {
+      const accessToken = generateAccessToken(user);
+      const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+      refreshTokens.push(refreshToken);
+      console.log(username + " has been authenticated!");
+      res.json({ accessToken, refreshToken });
+    } else {
+      res.status(403).send("Not allowed!");
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Server error!");
+  }
 });
 
 app.delete("/logout", (req, res) => {
